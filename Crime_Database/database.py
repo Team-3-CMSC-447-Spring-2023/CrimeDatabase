970b9c64e0946754e3526446e64762b8289eadaf
“TalhaVI09499” <vi09499@umbc.edu>
2023-05-17 04:10:12 -0400

    CTS-29 Create the Database: This database.py Python File is for CTS-14 and has added modifications in order to connect the database to the backend in order to properly generate the Heat Map to show all the actual data points.

--- Crime_Database/database.py
+++ Crime_Database/database.py
@@ -76,10 +76,17 @@ def insertWeapon(crime_data):
     weapon_array = np.array(weapon)
 
     for i in range(len(weapon_array)):
+        # Added a progress meter that is the number of inserted weapons compared to the total number of weapons to 
+        # help with knowing how long it will take.
+        print(f"Inserting Weapon: {i}/{len(weapon_array)}         ", end="\r")
+
         # Insert the Weapon ID represented as "weapon_id" in the "weapon" Table. In addition, insert the Weapon Name
         # represented as "weapon_name" in the "weapon" table.
         cursor.execute('INSERT INTO weapon (weapon_id,weapon_name) VALUES(%s, %s)', (i, weapon[i]))
         mydb.commit()
+
+     # The Print Statement adds a line break for readability.
+    print()
     return 0
 
 '''
@@ -100,7 +107,10 @@ def insertNeighborhood(crime_data):
 
     # need to replace 0,0 with the correct data sheets ******
     for i in range(len(neighborhood_array)):
-        print("#################################")
+        # Added a progress meter that is the number of inserted neighborhoods compared to the total number of 
+        # neighborhoods to help with knowing how long it will take.
+        print(f"Inserting Neighborhood: {i}/{len(neighborhood_array)}         ", end="\r")
+
         # Set the default Average Income Value represented by the "income" variable.
         income = 0
 
@@ -117,6 +127,7 @@ def insertNeighborhood(crime_data):
             try:
                 # Check if the "Community Statistical Area (2020)" and the Neighborhood name are the same.
                 if average_income.iloc[j]["Community Statistical Area (2020)"].lower() in neighborhood[i].lower():
+
                     # If the "Community Statistical Area (2020)" and the Neighborhood name are the same, then
                     # store the Average Income from 2016 to 2020 in the "income" Variable as a float.
                     income = float(average_income.iloc[j]["2016-2020"] or 0)
@@ -128,6 +139,7 @@ def insertNeighborhood(crime_data):
             try:
                 # Check if the food source, "neighborhood_name", and the actual neighborhood name are the same.
                 if food_sources.iloc[j]["neighborhood_name"].lower() in neighborhood[i].lower():
+
                     # If the food source, "neighborhood_name", and the actual neighborhood are the same, then
                     # store the District Number in the "district_num" Variable as an integer.
                     district_num = int(food_sources.iloc[j]["district_number"] or 0)
@@ -136,13 +148,16 @@ def insertNeighborhood(crime_data):
                     food_count = int(food_sources.iloc[j]["number_of_food_source_quantity_places"] or 0)
             except:
                 pass
-        print(neighborhood[i])
-        print("#################################")
+        
+
 
         # Insert all the data that was fetched from the Excel Microsoft Office Open Extensible Markup Language XML
         # Format Spreadsheet, .xlsx, and Comma-Separated Values CSV Files into a Database.
         cursor.execute('INSERT INTO neighborhood (neighborhood_name, district_number, average_income, food_source_quantity) VALUES(%s, %s, %s, %s)', (neighborhood[i], district_num, income, food_count))
         mydb.commit()
+
+    # The Print Statement adds a line break for readability.
+    print()
 
     # Updates all average_income of zeros to a default value of average_income of its district.
     for p in range(1, 15):
@@ -162,14 +177,17 @@ def insertCrime_type(crime_data):
     crime_type = crime_data[['CrimeCode', 'Description']].drop_duplicates(subset=['CrimeCode'])
     crime_type_array = np.array(crime_type)
 
-    print(crime_type_array)
-
-    # The crime_type_array contains tuples. The First Index of the tuple contains the Crime Type Name. The Second Index
-    # of the tuple contains the Crime Type Description.
+    # The crime_type_array contains tuples. The First Index of the tuple contains the Crime Type Name. The
+    # Second Index of the tuple contains the Crime Type Description.
     for i in range(len(crime_type_array)):
+        # Added a progress meter that is the number of inserted crime types compared to the total number of crime
+        # types to help with knowing how long it will take.
+        print(f"Inserting Crime Type: {i}/{len(crime_type_array)}         ", end="\r")
         # Insert the data into the "crime_type" Table.
         cursor.execute('INSERT INTO crime_type (type_name, type_Description) VALUES(%s, %s)', (crime_type_array[i][0], crime_type_array[i][1]))
         mydb.commit()
+    # The Print Statement adds a line break for readability.
+    print()
     return 0
 
 '''
@@ -180,32 +198,39 @@ def insertCrime(crime_data):
 
     crime_array = np.array(crime_data)
 
-    print(crime_array)
-    print(len(crime_array))
     # Get the data from weapon to find the correct weapon_id.
     query = "SELECT * FROM weapon"
     cursor.execute(query)
     result = cursor.fetchall()
+    weapons = {}
+    for weapon in result:
+        weapons[weapon[1]] = weapon[0]
 
     for i in range(len(crime_array)):
-        for j in range(len(result)):
-            # Check if the Weapon Name in the result array and "crime_array" are the same. Index j will
-            # have the Weapon ID.
-            if crime_array[i][5] == result[j][1]:
+        # Added a progress meter that is the number of inserted crimes compared to the total number of crimes to 
+        # help with knowing how long it will take.
+        print(f"Inserting Crime: {i}/{len(crime_array)}         ", end="\r")
+        
+        if crime_array[i][5] not in weapons:
+            continue
 
-                # Add Spacing between the Data and Time using Python's "split()" Function.
-                datetime = crime_array[i][1].split(" ")
+        # Add Spacing between the Data and Time using Python's "split()" Function.
+        datetime = crime_array[i][1].split(" ")
 
-                # If there is no latitude or longitude for an entry, then that entire entry will be ignored.
-                if crime_array[i][7] != 0 and crime_array[i][8] != 0:    
-                    # Fetch the time from the First Index that is Index 1 and then store it in the "time" Variable.
-                    cursor.execute('INSERT INTO crime (crime_id, neighborhood_name, weapon_id, type_name, crime_date, crime_time, latitude, longitude) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)',
-                                (i, crime_array[i][6], j, crime_array[i][2], datetime[0], datetime[1], crime_array[i][7],crime_array[i][8]))
-                    mydb.commit()
+        # If there is no latitude or longitude for an entry, then that entire entry will be ignored.
+        if crime_array[i][7] != 0 and crime_array[i][8] != 0:    
+            # Fetch the time from the First Index that is Index 1 and then store it in the "time" Variable.
+            cursor.execute('INSERT INTO crime (crime_id, neighborhood_name, weapon_id, type_name, crime_date, crime_time, latitude, longitude) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)',
+                        (i, crime_array[i][6], weapons[crime_array[i][5]], crime_array[i][2], datetime[0], datetime[1], crime_array[i][7],crime_array[i][8]))
+    mydb.commit()
+
     return 0
 
-
-if __name__ == '__main__':
+'''
+The Function "def populate_database() populates the database that takes about 20 minutes with the 560,038 crimes
+currently in the Comma-Separated Value CSV File "Part_1_Crime_Data.csv". 
+'''
+def populate_database():
     dropTables()
     createTables()
 
@@ -227,3 +252,6 @@ if __name__ == '__main__':
     insertNeighborhood(crime_data)
     insertCrime_type(crime_data)
     insertCrime(crime_data)
+
+if __name__ == '__main__':
+    populate_database()
